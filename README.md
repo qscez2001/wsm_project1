3be8800a8f38698e403dc044c2198f523ce34a3a
yuyuchen <qscez2001@gmail.com>
2020-04-02 21:31:11 +0800

    update

--- README.md
+++ README.md
@@ -11,11 +11,20 @@ TF
 
 use raw TF (term frequency)
 
+(use the default simple term count model in VectorSpace.py)
+
 IDF
 
 adjust the denominator to 1 + denominator
 math.log(len(self.documentVectors) / (1 + denominator))
 
+(see tfidf function in VectorSpace.py)
+
+
 Euclidean_distance
+
+(see euc_distance in util.py)
+
+other codes are all in main.py
 
 ![Image of distance](distance.png)
--- codes/util.py
+++ codes/util.py
@@ -18,12 +18,13 @@ def cosine(vector1, vector2):
 	""" related documents j and q are in the concept space by comparing the vectors :
 		cosine  = ( V1 * V2 ) / ||V1|| x ||V2|| """
 	return float(dot(vector1,vector2) / (norm(vector1) * norm(vector2)))
-
+'''
 def euclidean(vector1, vector2):
     vector1 = np.array(vector1)
     vector2 = np.array(vector2)
     # distance.euclidean(vector1, vector2)
     return norm(vector1 - vector2)
+'''
 
 def euc_distance(alist, blist):
     sum_of = 0
